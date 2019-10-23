5f976ec098299f8f519ceb90b84eb4e3099ba3b5
Tyler Long <tyler4long@gmail.com>
2019-10-23 16:11:28 -0700

    Separate demo for file saving

---
+++ examples/saveinbound.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/pion/webrtc/v2"
+	"github.com/zaf/g711"
+	"log"
+	"os"
+	"os/user"
+
+	"github.com/joho/godotenv"
+	"github.com/ringcentral/ringcentral-go"
+	sp "github.com/ringcentral/ringcentral-softphone-go"
+)
+
+func main() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = godotenv.Overload(usr.HomeDir + "/.env.prod")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	rc := ringcentral.RestClient{
+		ClientID:     os.Getenv("RINGCENTRAL_CLIENT_ID"),
+		ClientSecret: os.Getenv("RINGCENTRAL_CLIENT_SECRET"),
+		Server:       os.Getenv("RINGCENTRAL_SERVER_URL"),
+	}
+	rc.Authorize(
+		os.Getenv("RINGCENTRAL_USERNAME"),
+		os.Getenv("RINGCENTRAL_EXTENSION"),
+		os.Getenv("RINGCENTRAL_PASSWORD"),
+	)
+	softphone := sp.Softphone{
+		Rc: rc,
+	}
+	softphone.Register()
+
+	softphone.OnTrack = func(track *webrtc.Track) {
+		os.Remove("temp.raw")
+		f, err := os.OpenFile("temp.raw", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		for{
+			rtp, err := track.ReadRTP()
+			if err != nil {
+				log.Fatal(err)
+			}
+			f.Write(g711.DecodeUlaw(rtp.Payload))
+		}
+	}
+
+	softphone.WaitForIncomingCall()
+}
+
+// to play the saved audio:  play -b 16 -e signed -c 1 -r 8000 temp.raw
