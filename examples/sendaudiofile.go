4ac3bc5248084631e8e7be253e665cd09ea064e2
Tyler Long <tyler4long@gmail.com>
2019-10-23 17:17:50 -0700

    Try send audio file to remote

---
+++ examples/sendaudiofile.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/user"
+
+	"github.com/joho/godotenv"
+	"github.com/ringcentral/ringcentral-go"
+	sp "github.com/ringcentral/ringcentral-softphone-go"
+)
+
+func main() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = godotenv.Overload(usr.HomeDir + "/.env.prod")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	rc := ringcentral.RestClient{
+		ClientID:     os.Getenv("RINGCENTRAL_CLIENT_ID"),
+		ClientSecret: os.Getenv("RINGCENTRAL_CLIENT_SECRET"),
+		Server:       os.Getenv("RINGCENTRAL_SERVER_URL"),
+	}
+	rc.Authorize(
+		os.Getenv("RINGCENTRAL_USERNAME"),
+		os.Getenv("RINGCENTRAL_EXTENSION"),
+		os.Getenv("RINGCENTRAL_PASSWORD"),
+	)
+	softphone := sp.Softphone{
+		Rc: rc,
+	}
+	softphone.Register()
+
+	// todo: read audio file to remote
+
+	softphone.WaitForIncomingCall()
+}
