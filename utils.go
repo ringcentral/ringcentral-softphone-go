8af88dafbd0167d0d9c0f6c96fba71358e8d6c3e
Tyler Long <tyler4long@gmail.com>
2019-10-25 11:19:58 -0700

    Refactor by renaming

--- utils.go
+++ utils.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ringcentral/ringcentral-go/definitions"
 )
 
-func GenerateResponse(username, password, realm, method, uri, nonce string) string {
+func generateResponse(username, password, realm, method, uri, nonce string) string {
 	ha1 := md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", username, realm, password)))
 	ha2 := md5.Sum([]byte(fmt.Sprintf("%s:%s", method, uri)))
 	response := md5.Sum([]byte(fmt.Sprintf("%x:%s:%x", ha1, nonce, ha2)))
@@ -18,7 +18,7 @@ func generateAuthorization(sipInfo definitions.SIPInfoResponse, method, nonce st
 	return fmt.Sprintf(
 		`Digest algorithm=MD5, username="%s", realm="%s", nonce="%s", uri="sip:%s", response="%s"`,
 		sipInfo.AuthorizationId, sipInfo.Domain, nonce, sipInfo.Domain,
-		GenerateResponse(sipInfo.AuthorizationId, sipInfo.Password, sipInfo.Domain, method, "sip:"+sipInfo.Domain, nonce),
+		generateResponse(sipInfo.AuthorizationId, sipInfo.Password, sipInfo.Domain, method, "sip:"+sipInfo.Domain, nonce),
 	)
 }
 
@@ -26,7 +26,7 @@ func generateProxyAuthorization(sipInfo definitions.SIPInfoResponse, method, tar
 	return fmt.Sprintf(
 		`Digest algorithm=MD5, username="%s", realm="%s", nonce="%s", uri="sip:%s@%s", response="%s"`,
 		sipInfo.AuthorizationId, sipInfo.Domain, nonce, targetUser, sipInfo.Domain,
-		GenerateResponse(sipInfo.AuthorizationId, sipInfo.Password, sipInfo.Domain, method, "sip:"+targetUser+"@"+sipInfo.Domain, nonce),
+		generateResponse(sipInfo.AuthorizationId, sipInfo.Password, sipInfo.Domain, method, "sip:"+targetUser+"@"+sipInfo.Domain, nonce),
 	)
 }
 
