26f4b18ab14258295734d7e5c34c42482df0da72
Tyler Long <tyler4long@gmail.com>
2021-02-18 11:56:14 -0800

    github.com/ringcentral/ringcentral-go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ringcentral/ringcentral-softphone-go
 
 go 1.15
+
+require github.com/ringcentral/ringcentral-go v0.8.0
--- softphone_test.go
+++ softphone_test.go
@@ -1,11 +1,35 @@
 package softphone
 
 import (
-	"fmt"
+	"encoding/json"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/ringcentral/ringcentral-go"
 )
 
 func TestAuthorize(t *testing.T) {
-	softphone := Softphone{}
-	fmt.Println(softphone)
+	rc := ringcentral.RestClient{
+		ClientID:     os.Getenv("RINGCENTRAL_CLIENT_ID"),
+		ClientSecret: os.Getenv("RINGCENTRAL_CLIENT_SECRET"),
+		Server:       os.Getenv("RINGCENTRAL_SERVER_URL"),
+	}
+
+	rc.Authorize(ringcentral.GetTokenRequest{
+		GrantType: "password",
+		Username:  os.Getenv("RINGCENTRAL_USERNAME"),
+		Extension: os.Getenv("RINGCENTRAL_EXTENSION"),
+		Password:  os.Getenv("RINGCENTRAL_PASSWORD"),
+	})
+
+	bytes := rc.Post("/restapi/v1.0/client-info/sip-provision", strings.NewReader(`{"sipInfo":[{"transport":"WSS"}]}`))
+	var createSipRegistrationResponse ringcentral.CreateSipRegistrationResponse
+	json.Unmarshal(bytes, &createSipRegistrationResponse)
+
+	if len(createSipRegistrationResponse.SipInfo) <= 0 {
+		t.Error("No SipInfo")
+	}
+
+	rc.Revoke()
 }
